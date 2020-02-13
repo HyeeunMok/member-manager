6ef7216fa355499bc716b211a9593e88aad248d8
Hyeeun Mok <hyeeuni.mok@gmail.com>
2020-02-13 11:48:54 -0500

    add auth state and private routes

--- membermanager/frontend/src/reducers/index.js
+++ membermanager/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import members from './members';
 import errors from './errors';
 import messages from './messages';
+import auth from './auth';
 
 export default combineReducers({
   members,
   errors,
-  messages
+  messages,
+  auth
 });
